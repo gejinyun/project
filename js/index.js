2e5574c3d858d98cc8162cf73867e2a1543004c9
gjy <180144835@qq.com>
2017-11-17 15:09:29 +0800

    update

--- js/index.js
+++ js/index.js
@@ -1,179 +1,179 @@
-﻿$('#tel').focus();//打开页面手机输入框自动获取焦点
-var clear = document.getElementById('clear');
-var tel = document.getElementById('tel');
-var send = document.getElementById('send');
-var receive = document.getElementById('receive');
+﻿$(function(){
+	$('#tel').focus();//打开页面手机输入框自动获取焦点
+	var clear = document.getElementById('clear');
+	var tel = document.getElementById('tel');
+	var send = document.getElementById('send');
+	var receive = document.getElementById('receive');
 
-// 判断手机号是否合法
-var flag = true;
-var patt = /^1[34578]\d{9}$/;
-window.onkeyup =  function(){
-	if(tel.value==''){
-		clear.style.display='none';
-	}else{
-		clear.style.display='block';
-	};
-	//console.log(patt.test(tel.value))
-	if(patt.test(tel.value)){
-		flag = true;
-		send.style.color = '#1a1a1a';
-		send.style.borderColor = "#000";		
-	}else{
-		flag = false;
-		send.style.background = '#fff';
-		send.style.color = '#999';
-		send.style.borderColor= '#999';
+	// 判断手机号是否合法
+	var flag = true;
+	var patt = /^1[34578]\d{9}$/;
+	window.onkeyup =  function(){
+		if(tel.value==''){
+			clear.style.display='none';
+		}else{
+			clear.style.display='block';
+		};
+		//console.log(patt.test(tel.value))
+		if(patt.test(tel.value)){
+			flag = true;
+			send.style.color = '#1a1a1a';
+			send.style.borderColor = "#000";		
+		}else{
+			flag = false;
+			send.style.background = '#fff';
+			send.style.color = '#999';
+			send.style.borderColor= '#999';
+		}
 	}
-}
-// 失去焦点的时候一键清除键显示
-tel.onblur = function(){
-	if(tel.value==''){
-		clear.style.display='none';
-	}else{
-		clear.style.display='block';
+	// 失去焦点的时候一键清除键显示
+	tel.onblur = function(){
+		if(tel.value==''){
+			clear.style.display='none';
+		}else{
+			clear.style.display='block';
+		}
+		if(patt.test(tel.value)){
+			flag = true;
+			send.style.color = '#1a1a1a';
+			send.style.borderColor = "#000";		
+		}else{
+			flag = false;
+			send.style.background = '#fff';
+			send.style.color = '#999';
+			send.style.borderColor= '#999';
+		}
 	}
-	if(patt.test(tel.value)){
-		flag = true;
-		send.style.color = '#1a1a1a';
-		send.style.borderColor = "#000";		
-	}else{
-		flag = false;
-		send.style.background = '#fff';
-		send.style.color = '#999';
-		send.style.borderColor= '#999';
-	}
-}
 
-// 点击获取验证码
-send.addEventListener('touchend', function(){
-	$('#check').focus();
-	var value = tel.value;
-	var code = $('#send').html();
-	// 时间戳
-	var timestamp = new Date().getTime();
-	// 随机数
-	var Num="";
-	for(var i=0;i<10;i++){
-		Num+=Math.floor(Math.random()*10);
-	}
-	// md5加密
-	var sign = timestamp+Num+'apiSendSms';
-		end_sign = md5(sign);
-	if(code=='验证码'&&tel.value!==''){
-		if(flag){
-			// 请求验证码
-			$.ajax({
+	// 点击获取验证码
+	send.addEventListener('touchend', function(){
+		$('#check').focus();
+		var value = tel.value;
+		var code = $('#send').html();
+		// 时间戳
+		var timestamp = new Date().getTime();
+		// 随机数
+		var Num="";
+		for(var i=0;i<10;i++){
+			Num+=Math.floor(Math.random()*10);
+		}
+		// md5加密
+		var sign = timestamp+Num+'apiSendSms';
+			end_sign = md5(sign);
+		if(code=='验证码'&&tel.value!==''){
+			if(flag){
+				// 请求验证码
+				$.ajax({
+					url: 'http://test.php.yihaojishi.com:8082/yhjs-tech/api/client/api.php',
+					type:'POST',
+					async:true,
+					data: {
+						Action:'apiSendSms',
+						phone:value,
+						timestamp:timestamp,
+						uniquestr:Num,
+						sign:end_sign
+					},
+					success:function (data){
+						var res = JSON.parse(data);
+						if (res.code == '0') {
+							$('.right_num').html(res.msg);
+							$('.right_num').show();
+							setTimeout(function(){
+								$('.right_num').hide();
+							}, 1000)
+						}else{
+							$('.error_system').html(res.msg);
+							$('.error_system').show();
+							setTimeout(function(){
+								$('.error_system').hide();
+							}, 1000)
+						}
+					}
+				})
+				// 倒计时
+			    i = 60;
+				$('#send').html(i+'s');
+			    end = setInterval(function(){
+			        i--;
+			        if(i==0){
+			        	clearInterval(end);
+			        	$('#send').html('验证码');
+			        }else{
+			        	$('#send').html(i+'s');
+			        }
+				}, 1000)
+				
+			}
+
+		}else if(!flag){
+			clearInterval(end);
+			$('#send').html('验证码');
+			$('.error_num').show();
+			setTimeout(function(){
+				$('.error_num').hide();
+			}, 1000)
+		}
+	})
+	// 一键清除
+	clear.addEventListener("touchstart",function(){
+		tel.value = '';
+		this.style.display = 'none';
+		clearInterval(end);
+		$('#send').html('验证码');
+	})
+
+	// 点击领取
+	var check = document.getElementById('check');
+	receive.addEventListener('touchstart', function(){
+		var code = check.value;
+		var value = tel.value;
+		// 时间戳
+		var timestamp2 = new Date().getTime();
+		// console.log(timestamp2);
+		var Num2="";
+		for(var i=0;i<10;i++){
+			Num2+=Math.floor(Math.random()*10);
+		}
+		// console.log(Num2);
+		var sign2 = timestamp2+Num2+'apiCheckCode';
+			end_sign2 = md5(sign2);
+		// console.log(end_sign2);
+		var machine_id = document.getElementById('machine_id').value;
+		if(tel.value!==''&&check.value!==''){
+			$.ajax({	
 				url: 'http://test.php.yihaojishi.com:8082/yhjs-tech/api/client/api.php',
-				type:'POST',
-				async:true,
+				type: 'POST',
 				data: {
-					Action:'apiSendSms',
+					Action:'apiCheckCode',
 					phone:value,
-					timestamp:timestamp,
-					uniquestr:Num,
-					sign:end_sign
+					code:code,
+					timestamp:timestamp2,
+					uniquestr:Num2,
+					sign:end_sign2
 				},
 				success:function (data){
 					var res = JSON.parse(data);
+					// console.log(res.code)
 					if (res.code == '0') {
-						$('.right_num').html(res.msg);
-						$('.right_num').show();
-						setTimeout(function(){
-							$('.right_num').hide();
-						}, 1000)
+						window.location.href="http://test.php.yihaojishi.com:8082/yhjs-tech/api/client/api.php?Action=apiGetGood&machine_id="+machine_id+"&phone="+value;
 					}else{
-						$('.error_system').html(res.msg);
-						$('.error_system').show();
+						$('.error_code').html(res.msg);
+						$('.error_code').show();
 						setTimeout(function(){
-							$('.error_system').hide();
+							$('.error_code').hide();
 						}, 1000)
 					}
 				}
 			})
-			// 倒计时
-		    i = 60;
-			$('#send').html(i+'s');
-		    end = setInterval(function(){
-		        i--;
-		        if(i==0){
-		        	clearInterval(end);
-		        	$('#send').html('验证码');
-		        }else{
-		        	$('#send').html(i+'s');
-		        }
+		}else{
+			$('.emptyParam').show();
+			setTimeout(function(){
+				$('.emptyParam').hide();
 			}, 1000)
-			
-		}
-
-	}else if(!flag){
-		clearInterval(end);
-		$('#send').html('验证码');
-		$('.error_num').show();
-		setTimeout(function(){
-			$('.error_num').hide();
-		}, 1000)
-	}
-})
-// 一键清除
-clear.addEventListener("touchstart",function(){
-	tel.value = '';
-	this.style.display = 'none';
-	clearInterval(end);
-	$('#send').html('验证码');
-})
-
-// 点击领取
-var check = document.getElementById('check');
-receive.addEventListener('touchstart', function(){
-	var code = check.value;
-	var value = tel.value;
-	// 时间戳
-	var timestamp2 = new Date().getTime();
-	// console.log(timestamp2);
-	var Num2="";
-	for(var i=0;i<10;i++){
-		Num2+=Math.floor(Math.random()*10);
-	}
-	// console.log(Num2);
-	var sign2 = timestamp2+Num2+'apiCheckCode';
-		end_sign2 = md5(sign2);
-	// console.log(end_sign2);
-	var machine_id = document.getElementById('machine_id').value;
-	if(tel.value!==''&&check.value!==''){
-		$.ajax({	
-			url: 'http://test.php.yihaojishi.com:8082/yhjs-tech/api/client/api.php',
-			type: 'POST',
-			data: {
-				Action:'apiCheckCode',
-				phone:value,
-				code:code,
-				timestamp:timestamp2,
-				uniquestr:Num2,
-				sign:end_sign2
-			},
-			success:function (data){
-				var res = JSON.parse(data);
-				// console.log(res.code)
-				if (res.code == '0') {
-					window.location="http://test.php.yihaojishi.com:8082/yhjs-tech/api/client/api.php?Action=apiGetGood&machine_id="+machine_id+"&phone="+value;
-				}else{
-					$('.error_code').html(res.msg);
-					$('.error_code').show();
-					setTimeout(function(){
-						$('.error_code').hide();
-					}, 1000)
-				}
-			}
-		})
-	}else{
-		$('.emptyParam').show();
-		setTimeout(function(){
-			$('.emptyParam').hide();
-		}, 1000)
-	}	
-})
-// 小山动画
-$(function(){
+		}	
+	})
+	// 小山动画，应该封装一下
 	var css={marginTop: "1.3rem"}
 	$('.szs').animate(css,
 		4000,callback);
@@ -205,4 +205,5 @@ $(function(){
             css3.marginTop='3.6rem';
 		$('.sy').animate(css3,3000,callback3); 
 	}
-})
+	
+})	
--- js/success.js
+++ js/success.js
@@ -1,52 +1,51 @@
-if(window.localStorage){
-	var bgcolor = localStorage.getItem("color");
-	var awardName = localStorage.getItem("name");
-	var con = document.getElementById("con");
-	var word = document.getElementById("word");
-	con.style.background = bgcolor;
-	word.innerHTML = awardName;
-	//console.log(bgcolor);
-}
+$(function(){
+	
+	var img_bg = document.getElementById('img_bg');
+	var img_hb = document.getElementById('img_hb');
+	var img_hbdk = document.getElementById('img_hbdk');
+	var share = document.getElementById('share');
+	var open = document.getElementById('open');
 
-var img_bg = document.getElementById('img_bg');
-var img_hb = document.getElementById('img_hb');
-var img_hbdk = document.getElementById('img_hbdk');
-var share = document.getElementById('share');
-var open = document.getElementById('open');
-
-img_hbdk.style.display = "none";
-share.style.display = "none";
-
-// 红包跳出
-setTimeout(function(){
-	img_bg.style.display = "block";
-}, 2000)
-
-// 打开红包
-open.addEventListener('touchend',function(){
-	img_hb.style.display = "none";
-	img_hbdk.style.display = "block";
-	// $.ajax({
-	// 	url: '/path/to/file',
-	// 	type: 'POST',
-	// 	data: {param1: 'value1'},
-	// })	
-})
-
-$('.close').click(function(){
-	img_bg.style.display = "none";
 	img_hbdk.style.display = "none";
-	share.style.display = "block";
-})
-// 点击分享页面消失
-var share = document.getElementById('share');
-share.addEventListener('touchend',function(){
-	this.style.display = 'none';
-})
+	share.style.display = "none";
 
-$('#use_btn').click(function(){
-	window.location = 'http://download.yihaojishi.com/find.html';
-})
-$('.btn').click(function(){
-	window.location = 'http://download.yihaojishi.com/find.html';
-})
+	// 上个页面抽奖结果接应
+	if(window.localStorage){
+		var bgcolor = localStorage.getItem("color");
+		var awardName = localStorage.getItem("name");
+		var con = document.getElementById("con");
+		var word = document.getElementById("word");
+			con.style.background = bgcolor;
+			word.innerHTML = awardName;
+			//console.log(bgcolor);
+	}
+
+	// 红包弹出
+	setTimeout(function(){
+		img_bg.style.display = "block";
+	}, 2000)
+
+	// 打开红包
+	open.addEventListener('touchend',function(){
+		img_hb.style.display = "none";
+		img_hbdk.style.display = "block";	
+	})
+
+	$('.close').click(function(){
+		img_bg.style.display = "none";
+		img_hbdk.style.display = "none";
+		share.style.display = "block";
+	})
+	// 点击分享页面消失
+	var share = document.getElementById('share');
+	share.addEventListener('touchend',function(){
+		this.style.display = 'none';
+	})
+
+	$('#use_btn').click(function(){
+		window.location.href = 'http://download.yihaojishi.com/find.html';
+	})
+	$('.btn').click(function(){
+		window.location.href = 'http://download.yihaojishi.com/find.html';
+	})
+})	
